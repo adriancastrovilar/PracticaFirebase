158b666efb06d4bf4cdc3127a67b919624b82112
adriancastrovilar <adrian.castrov@alumnos.upm.es>
2023-10-28 12:47:44 +0200

    Enhanced AuthUiActivity layout and funcionalities

--- app/src/main/java/com/firebase/uidemo/util/ConfigurationUtils.java
+++ app/src/main/java/com/firebase/uidemo/util/ConfigurationUtils.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 
 import com.firebase.ui.auth.AuthUI;
-import com.firebase.uidemo.R;
 import com.google.firebase.auth.ActionCodeSettings;
 
 import java.util.ArrayList;
@@ -19,27 +18,9 @@ public final class ConfigurationUtils {
         throw new AssertionError("No instance for you!");
     }
 
-    public static boolean isGoogleMisconfigured(@NonNull Context context) {
-        return AuthUI.UNCONFIGURED_CONFIG_VALUE.equals(
-                context.getString(R.string.default_web_client_id));
-    }
-
-    public static boolean isFacebookMisconfigured(@NonNull Context context) {
-        return AuthUI.UNCONFIGURED_CONFIG_VALUE.equals(
-                context.getString(R.string.facebook_application_id));
-    }
-
     @NonNull
     public static List<AuthUI.IdpConfig> getConfiguredProviders(@NonNull Context context) {
         List<AuthUI.IdpConfig> providers = new ArrayList<>();
-
-        if (!isGoogleMisconfigured(context)) {
-            providers.add(new AuthUI.IdpConfig.GoogleBuilder().build());
-        }
-
-        if (!isFacebookMisconfigured(context)) {
-            providers.add(new AuthUI.IdpConfig.FacebookBuilder().build());
-        }
 
         ActionCodeSettings actionCodeSettings = ActionCodeSettings.newBuilder()
                 .setAndroidPackageName("com.firebase.uidemo", true, null)
@@ -52,12 +33,6 @@ public final class ConfigurationUtils {
                 .enableEmailLinkSignIn()
                 .setActionCodeSettings(actionCodeSettings)
                 .build());
-
-        providers.add(new AuthUI.IdpConfig.TwitterBuilder().build());
-        providers.add(new AuthUI.IdpConfig.PhoneBuilder().build());
-        providers.add(new AuthUI.IdpConfig.MicrosoftBuilder().build());
-        providers.add(new AuthUI.IdpConfig.YahooBuilder().build());
-        providers.add(new AuthUI.IdpConfig.AppleBuilder().build());
 
         return providers;
     }
